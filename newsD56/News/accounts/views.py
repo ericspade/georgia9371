37055f646c3e220b9efcbec42fb44d6af5c5a67c
Александр Котенков <gtna8e6@gmail.com>
2022-06-24 20:15:55 -0600

    D5.6 homework

---
+++ newsD56/News/accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'protect/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_Authors'] = not self.request.user.groups.filter(name='Authors').exists()
+        return context
